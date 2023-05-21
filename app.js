bfc208ef8dfce74ee6bbe8e65c94fdee650ae4f7
Ren Lee <x7368907@gmail.com>
2023-05-21 14:07:12 +0800

    ok

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require('mongoose');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const homeStartingContent ="嗨，很高興能夠和你分享我的面試經歷。我是一位具有網頁開發背景的前端工程師。我對網頁開發充滿熱情，我具備的前端技能，包括HTML、CSS和JavaScript。我熟悉常用的前端框架，如Vue.js，並能夠運用這些框架來開發具有高度互動性和優秀用戶體驗的網站，以下是我面試過的一些公司，分享給想轉職前端的夥伴參考，希望大家都能順利轉職成功，一起朝軟體工程師的方向努力";
+const aboutContent = "我叫李志仁，對於網頁開發擁有濃厚興趣，並熱愛學習的前端開發者，具有1年左右經驗，專注於開發和維護具有高互動性的網站。熟練掌握HTML、CSS、JavaScript，並擅長使用Vue前端框架。對寫出高維護性的程式碼充滿熱忱，致力於不斷學習最佳實踐並提升技能水平。除了技術能力，我還重視不斷學習和成長。我經常追蹤最新的前端發展趨勢，參加技術研討會和工作坊，並努力保持自己的技能與行業最佳實踐保持同步。";
+const contactContent = "在我的經歷中，我曾參與過的項目：1. 物流Web app開發 2.活動網站開發，我喜歡和團隊合作，並能夠有效地與設計師、後端工程師和產品經理協作。我擁有解決問題的能力，並在壓力下能夠保持冷靜並交付高質量的工作。";
 const app = express();
 
 
@@ -57,9 +56,9 @@ app.get("/contact", function(req,res){
   res.render("contact",{contactContent: contactContent});
 });
 
-app.get("/compose", function(req,res){
+app.get("/compose",function(req,res){
   res.render("compose");
-});
+})
 
 app.post("/compose", function(req,res){
 
@@ -91,7 +90,7 @@ app.get("/posts/:postId", (req,res) => {
   }).catch((e) => {
     console.log(e);
   })
-})
+});
 
 
 // app.get("/posts/:postName",function(req,res){
